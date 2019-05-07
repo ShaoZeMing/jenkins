351f631573e2f8104ecdb35b208430d02bbb4216
shaozeming <szm19920426@gmail.com>
2019-05-07 16:38:20 +0800

    修改部署脚本

--- deploy-laravel.sh
+++ deploy-laravel.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
-# 一个示例部署laravel 项目 shell脚本 项目地址：https://github.com/ShaoZeMing/laravel-dingo-jwt-signature-apidoc-demo
-
 #需要针对服务器进行配置的变量
+
 OBJECT_ROOT_DIR='/home/deplay/laravel-demo'   #服务器项目部署根目录
-LINK_DIRS=('storage')                       #项目下不需要更新替换的静态目录,数组
+LINK_DIRS=('storage')                         #项目下不需要更新替换的静态目录,数组
+RELEASES_NUM=3                                #保留几个版本
 
 #创建基础目录
 OBJECT_SHARED_DIR=${OBJECT_ROOT_DIR}"/shared"          #静态文件目录
@@ -43,10 +42,26 @@ php artisan clear-compiled && php artisan optimize
 php artisan config:clear && php artisan config:cache
 php artisan route:clear && php artisan route:cache
 
-cp apidoc_development.json apidoc.json
 
 
 #部署结束后切换软连接
 [ -L  ${OBJECT_CURRENT_DIR} ] &&  rm -rf ${OBJECT_CURRENT_DIR}
 ln -nfs  ${RELEASES_ID_DIR} ${OBJECT_CURRENT_DIR}
 
+
+
+#根据部署个数，删除超过的之前版本，删除文件更改需谨慎===================
+#ls -l:目的是按行列出文件
+#grep "act-*":筛选出包含act-为开头的文件
+#wc -l:统计行数（也就相当于统计有多少个这样的文件）
+#-gt :大于
+#ls -rt:把文件按从旧到新的顺序排列
+#head -n1:选取排列的头两个文件，这个数字2可以任意的改变
+#rm -r:移除目录
+ cd ${OBJECT_RELEASES_DIR}
+ pwd
+ if [ $(ls -l | wc -l) -gt ${RELEASES_NUM} ]
+ then
+     echo "files > ${RELEASES_NUM}"
+     rm -r $(ls -rt | head -n1)
+ fi
